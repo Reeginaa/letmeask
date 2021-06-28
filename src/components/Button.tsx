8aef1183c48d4bd554efe73e2fa8d82db37aed9b
Maria Regina Cerbaro <172979@upf.br>
2021-06-28 17:22:15 -0300

    continuação aula 2

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,26 +1,15 @@
-//tipagem para passar propriedades
-//o ponto de interrogação após o text faz com que a propriedade seja opcional
-/*type ButtonProps = {
-  children?: String;
-}*/
+import { ButtonHTMLAttributes } from "react";
+import '../styles/button.scss';
 
-import { useState } from "react";
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-export function Button() {
-  //let counter = 0; //let deixa mudar
-  const [counter, setCounter] = useState(0);
 
-  function increment() {
-    setCounter(counter + 1);
-    //console.log(counter);
-  }
+export function Button(props: ButtonProps) {
 
   return (
-    <button onClick={increment}>
-      {counter}
+    <button className="button" {...props} >
+
     </button>
   )
 }
-//imutabilidade - a partir do momento que uma variável foi criada dentro do
-//estado de um componente ela não sofre alterações, sempre criamos uma nova 
-//informação baseada na informação que tinhamos
+

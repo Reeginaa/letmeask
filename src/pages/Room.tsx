783b0efefb962a6529c5e9ff2bb53400f483a839
Maria Regina Cerbaro <172979@upf.br>
2021-07-21 17:14:45 -0300

    Aula 4 - 20 min

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -6,6 +6,7 @@ import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
+import { Question } from '../components/Question';
 import { useAuth } from '../hooks/useAuth';
 import { database } from '../services/firebase';
 
@@ -126,7 +127,17 @@ export function Room() {
           </div>
         </form>
 
-        {JSON.stringify(questions)}
+        <div className="question-list">
+          {questions.map(question => {
+            return (
+              <Question
+                key={question.id}
+                content={question.content}
+                author={question.author}
+              />
+            );
+          })}
+        </div>
       </main>
     </div>
   );
